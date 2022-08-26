8456ea78f81ec103ef86221fd6aaa5991563a322
Mohammed Sohail <sohailsameja@gmail.com>
2022-08-26 07:23:26 +0000

    patch: cameroon specific dashboard queries

--- internal/dashboard/charts.go
+++ internal/dashboard/charts.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/georgysavva/scany/pgxscan"
+	"github.com/jackc/pgx/v4"
 	"github.com/labstack/echo/v4"
 )
 
@@ -18,15 +19,25 @@ type lineChartRes struct {
 func handleNewRegistrations(c echo.Context) error {
 	var (
 		api = c.Get("api").(*api)
+		qP  = c.QueryParams()
 
+		rows pgx.Rows
+		err  error
 		data []lineChartRes
 	)
 
-	from, to := date_range.ParseDateRange(c.QueryParams())
+	from, to := date_range.ParseDateRange(qP)
 
-	rows, err := api.db.Query(context.Background(), api.q["new-user-registrations"], from, to)
-	if err != nil {
-		return err
+	if qP.Get("country") == "cmr" {
+		rows, err = api.db.Query(context.Background(), api.q["new-user-registrations-cmr"], from, to)
+		if err != nil {
+			return err
+		}
+	} else {
+		rows, err = api.db.Query(context.Background(), api.q["new-user-registrations"], from, to)
+		if err != nil {
+			return err
+		}
 	}
 
 	if err := pgxscan.ScanAll(&data, rows); err != nil {
@@ -39,15 +50,25 @@ func handleNewRegistrations(c echo.Context) error {
 func handleTransactionsCount(c echo.Context) error {
 	var (
 		api = c.Get("api").(*api)
+		qP  = c.QueryParams()
 
+		rows pgx.Rows
+		err  error
 		data []lineChartRes
 	)
 
-	from, to := date_range.ParseDateRange(c.QueryParams())
+	from, to := date_range.ParseDateRange(qP)
 
-	rows, err := api.db.Query(context.Background(), api.q["transactions-count"], from, to)
-	if err != nil {
-		return err
+	if qP.Get("country") == "cmr" {
+		rows, err = api.db.Query(context.Background(), api.q["transactions-count-cmr"], from, to)
+		if err != nil {
+			return err
+		}
+	} else {
+		rows, err = api.db.Query(context.Background(), api.q["transactions-count"], from, to)
+		if err != nil {
+			return err
+		}
 	}
 
 	if err := pgxscan.ScanAll(&data, rows); err != nil {
--- queries/dashboard.sql
+++ queries/dashboard.sql
@@ -12,6 +12,22 @@ GROUP BY date_range.day
 ORDER BY date_range.day
 LIMIT 730;
 
+-- name: new-user-registrations-cmr
+-- This is a patch to support CMR dashboard
+-- This query generates a date range and left joins the users table to include days with no registrations
+-- Produces x, y results for displaying on a line chart
+WITH date_range AS (
+    SELECT day::date FROM generate_series('08-01-2022', '08-26-2022', INTERVAL '1 day') day
+)
+
+SELECT date_range.day AS x, COUNT(users.id) AS y
+FROM date_range
+LEFT JOIN users ON date_range.day = CAST(users.date_registered AS date)
+WHERE users.phone_number LIKE '237%'
+GROUP BY date_range.day
+ORDER BY date_range.day
+LIMIT 730;
+
 -- name: transactions-count
 -- This query generates a date range and left joins the transactions table to include days with no transactions
 -- Produces x, y results for displaying on a line chart
@@ -27,6 +43,29 @@ FROM date_range
 LEFT JOIN transactions ON date_range.day = CAST(transactions.date_block AS date)
 AND transactions.sender_address NOT IN (SELECT sys_address FROM exclude) AND transactions.recipient_address NOT IN (SELECT sys_address FROM exclude)
 AND transactions.success = true
+GROUP BY date_range.day
+ORDER BY date_range.day
+LIMIT 730;
+
+-- name: transaction-count-cmr
+-- This is a patch to support CMR dashboard
+-- This query generates a date range and left joins the transactions table to include days with no transactions
+-- Produces x, y results for displaying on a line chart
+WITH date_range AS (
+    SELECT day::date FROM generate_series('08-01-2022', '08-26-2022', INTERVAL '1 day') day
+),
+exclude AS (
+    SELECT sys_address FROM sys_accounts WHERE sys_address IS NOT NULL
+),
+cmr_tokens AS (
+  	SELECT token_address from tokens WHERE token_symbol IN ('MBIP')
+)
+SELECT date_range.day AS x, COUNT(transactions.id) AS y
+FROM date_range
+LEFT JOIN transactions ON date_range.day = CAST(transactions.date_block AS date)
+AND transactions.sender_address NOT IN (SELECT sys_address FROM exclude) AND transactions.recipient_address NOT IN (SELECT sys_address FROM exclude)
+AND transactions.success = true
+AND transactions.token_address IN (SELECT token_address FROM cmr_tokens)
 GROUP BY date_range.day
 ORDER BY date_range.day
 LIMIT 730;
